a9d1c696b222cbc10735b229954a2f013ed5b7bd
Innocent Hassan <Innocent8475@users.noreply.github.com>
2026-04-07 21:26:03 +0100

    Initial push from current directory

---
+++ app/src/main/assets/www/cordova_plugins.js
@@ -0,0 +1,24 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+  module.exports = [
+    {
+      "id": "cordova-plugin-statusbar.statusbar",
+      "file": "plugins/cordova-plugin-statusbar/www/statusbar.js",
+      "pluginId": "cordova-plugin-statusbar",
+      "clobbers": [
+        "window.StatusBar"
+      ]
+    },
+    {
+      "id": "cordova-plugin-navigationbar-color.navigationbar",
+      "file": "plugins/cordova-plugin-navigationbar-color/www/navigationbar.js",
+      "pluginId": "cordova-plugin-navigationbar-color",
+      "clobbers": [
+        "window.NavigationBar"
+      ]
+    }
+  ];
+  module.exports.metadata = {
+    "cordova-plugin-statusbar": "4.0.0",
+    "cordova-plugin-navigationbar-color": "0.0.8"
+  };
+});
